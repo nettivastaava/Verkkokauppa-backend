703679f2d4c6bd4d828d8ee0e2425715cd53df1c
Valtteri Andström <valtteri.andstrom@gmail.com>
2021-07-23 18:41:55 +0300

    refactoring

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express')
 const expressGraphQL = require('express-graphql');
 const cors = require('cors')
 const Product = require('./models/product')
+const { typeDefs } = require('./models/typeDefs')
 const User = require('./models/user')
 const Comment = require('./models/comment')
 const config = require('./utils/config')
@@ -30,95 +31,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
-const typeDefs = gql`
-  type Product {
-    name: String!
-    price: Float!
-    quantity: Int!
-    categories: [String!]!
-    id: ID!
-    description: String 
-    comments: [Comment]!
-    units_sold: Int!
-  }
-
-  type Query {
-    productCount: Int!
-    allProducts(category: String): [Product]!
-    allCategories: [String]!
-    me: User
-    findProduct(name: String): Product
-    allComments(product: String): [Comment]!
-    totalPrice: Float!
-  }
-
-  type User {
-    username: String!
-    password: String!
-    id: ID!
-    cart: [ProductInCart]!
-  }
-
-  type ProductInCart {
-    productName: String!
-    price: Float!
-    amount: Int!
-  }
-
-  type Comment {
-    user: String!
-    product: String!
-    content: String!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type Mutation {
-    addProduct(
-      name: String!
-      price: Float!
-      quantity: Int!
-      categories: [String!]!
-      description: String
-      comments: [String]!
-      units_sold: Int!
-    ): Product
-    increaseQuantity(    
-      name: String!    
-      quantity: Int! 
-    ): Product
-    decreaseQuantity(
-      name: String!
-      quantity: Int!
-    ): Product
-    createUser(
-      username: String!
-      password: String!
-      passwordConf: String!
-    ): User
-    login(
-      username: String!
-      password: String!
-    ): Token
-    addComment(
-      product: String!
-      user: String!
-      content: String!
-    ): Comment
-    addToCart(
-      productName: String!
-      price: Float!
-    ): User
-    removeFromCart(
-      productName: String!
-    ): User
-    checkout: User
-  }
-`
 
 const resolvers = {
   Query: {
---
+++ models/typeDefs.js
@@ -0,0 +1,92 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Product {
+    name: String!
+    price: Float!
+    quantity: Int!
+    categories: [String!]!
+    id: ID!
+    description: String 
+    comments: [Comment]!
+    units_sold: Int!
+  }
+
+  type Query {
+    productCount: Int!
+    allProducts(category: String): [Product]!
+    allCategories: [String]!
+    me: User
+    findProduct(name: String): Product
+    allComments(product: String): [Comment]!
+    totalPrice: Float!
+  }
+
+  type User {
+    username: String!
+    password: String!
+    id: ID!
+    cart: [ProductInCart]!
+  }
+
+  type ProductInCart {
+    productName: String!
+    price: Float!
+    amount: Int!
+  }
+
+  type Comment {
+    user: String!
+    product: String!
+    content: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Mutation {
+    addProduct(
+      name: String!
+      price: Float!
+      quantity: Int!
+      categories: [String!]!
+      description: String
+      comments: [String]!
+      units_sold: Int!
+    ): Product
+    increaseQuantity(    
+      name: String!    
+      quantity: Int! 
+    ): Product
+    decreaseQuantity(
+      name: String!
+      quantity: Int!
+    ): Product
+    createUser(
+      username: String!
+      password: String!
+      passwordConf: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+    addComment(
+      product: String!
+      user: String!
+      content: String!
+    ): Comment
+    addToCart(
+      productName: String!
+      price: Float!
+    ): User
+    removeFromCart(
+      productName: String!
+    ): User
+    checkout: User
+  }
+`
+
+module.exports = { typeDefs }
